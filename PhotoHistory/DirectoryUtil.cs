ec7d3ed3d42a6456f6bcb9a9b935d9f86bc94c9c
vokabud <etecture.dubakov@extaccount.com>
2025-03-30 12:08:49 +0300

    Add directory util

---
+++ PhotoHistory/DirectoryUtil.cs
@@ -0,0 +1,23 @@
+﻿namespace PhotoHistory;
+
+internal class DirectoryUtil
+{
+    public static string[] GetFilesByExtension(string directory, string extension)
+    {
+        if (!Directory.Exists(directory))
+        {
+            throw new DirectoryNotFoundException();
+        }
+
+        if (string.IsNullOrEmpty(extension))
+        {
+            throw new ArgumentException(null, nameof(extension));
+        }
+
+        return Directory
+            .GetFiles(
+                directory,
+                $"*.{extension}",
+                SearchOption.AllDirectories);
+    }
+}
---
+++ PhotoHistory/Program.cs
@@ -0,0 +1,16 @@
+﻿using PhotoHistory;
+
+Console.WriteLine("Start.");
+
+var path = @"G:\Photo";
+var extension = "jpg";
+
+var files = DirectoryUtil.GetFilesByExtension(path, extension);
+
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+
+Console.WriteLine("End.");
